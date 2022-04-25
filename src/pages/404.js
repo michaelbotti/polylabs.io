acb7f7f8412d83c08a12f92a75eb654e0a1d61d2
guavajellyaaron <aaron@guavajellyco.com>
2022-04-25 13:52:19 -0700

    remove unused pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
-import Layout from "../components/Sections/Layout";
-import Loader from "../components/Sections/Loader";
-import ErrorPage from "../components/404/ErrorPage";
-import ToTop from "../components/Sections/ToTop";
+import Layout from '../components/Sections/Layout';
+import Loader from '../components/Sections/Loader';
+import ErrorPage from '../components/404/ErrorPage';
+import ToTop from '../components/Sections/ToTop';
 
 const Custom404 = () => (
-
-    <Layout 
-        pageTitle="Naxos - Ooops..." 
-        colorSchema="../colors/blue.css"
-    >
+    <Layout pageTitle="Kora Labs - Page Not Found" colorSchema="../colors/blue.css">
         <Loader />
         <ErrorPage />
         <ToTop />
-
     </Layout>
-
-)
+);
 
 export default Custom404;
